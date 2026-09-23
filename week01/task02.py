e73e9c38969c9cc6ea3d2081304a499548d649bd
drawcodeboy <20rnjsekdns@naver.com>
2026-09-24 01:18:23 +0900

    [Modify] Rename each script "day" -> "task"

--- week01/task02.py
+++ week01/task02.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, UploadFile, File
 app = FastAPI()
 
 @app.post("/upload-image")
-def upload_image(file: UploadFile = File(...)):
+async def upload_image(file: UploadFile = File(...)):
     return {
         "filename": file.filename,
         "content_type": file.content_type
